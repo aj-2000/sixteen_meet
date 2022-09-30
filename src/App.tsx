1e41892765fb8d8bb0c95c5748e56320619b2735
adi-coder2000 <contact.quicktech@gmail.com>
2022-10-01 01:54:09 +0530

    ReactJS App Initialized

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+export default function BasicButtons() {
+  return (
+    <div className='text-center text-orange-500'>
+      Sixteen Meet
+    </div>
+  );
+}
